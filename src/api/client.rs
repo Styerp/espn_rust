b84ff93537a8b7ec9340a723898f83ae4f24c2c9
Styerp <styerp@amazon.com>
2023-09-27 16:34:41 -0700

    Initial commit.
    Working 'private league' client with one function.

---
+++ src/api/client.rs
@@ -0,0 +1,40 @@
+use reqwest::{
+    cookie::Jar,
+    header::{HeaderMap, COOKIE},
+    Client,
+};
+const ESPN_FF_BASE_URL: &str = "https://fantasy.espn.com/apis/v3/games/ffl/seasons";
+
+pub struct EspnClient<'a> {
+    pub client: Client,
+    pub league_id: i32,
+    pub base_url: &'a str,
+}
+
+impl<'a> EspnClient<'a> {
+    pub fn build(swid: &str, espn_s2: &str, league_id: i32) -> EspnClient<'a> {
+        let mut headers = HeaderMap::new();
+        headers.insert(COOKIE, format!("SWID={swid}").parse().unwrap());
+        headers.insert(COOKIE, format!("espn_s2={espn_s2}").parse().unwrap());
+        let cookie_store: Jar = Jar::default();
+        cookie_store.add_cookie_str(
+            format!("SWID={swid}; espn_s2={espn_s2}").as_str(),
+            &ESPN_FF_BASE_URL.parse().unwrap(),
+        );
+        let client_builder = Client::builder()
+            .default_headers(headers.clone())
+            .connection_verbose(true)
+            .cookie_store(true)
+            .cookie_provider(cookie_store.into())
+            .build();
+        let client = match client_builder {
+            Ok(c) => c,
+            Err(e) => panic!("Failed to construct client. Aborting. {e}"),
+        };
+        EspnClient {
+            client,
+            league_id,
+            base_url: ESPN_FF_BASE_URL,
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod api;
+// use client::build_client;
+// use api::league_data::get_league_data;

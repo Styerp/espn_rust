396c3f885934839025e67db49226fddc2d9a511a
Styerp <styerp@amazon.com>
2023-10-06 16:06:03 -0700

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use bench_king::api::{
     client::EspnClient,
     matchup::Roster,
-    team::{TeamId, TeamResponse},
+    team::{TeamId, TeamResponse}, id_maps::PositionId,
 };
-use std::env;
+use std::{env, collections::HashMap};
+
 static LEAGUE_ID: i32 = 111368805;
 const SEASON_ID: i16 = 2023;
 static SWID: &str = "{8A5D0331-9FAB-4A5F-BA5F-1F52DECD4646}";
@@ -11,17 +12,15 @@ static ESPN_S2: &str = "AEAE3uggnFrxe%2Fulf%2FN3Y5DZpkDwrPnG2zpRTd3z3m%2B7YJ2Fmu
 
 #[tokio::main]
 async fn main() {
-    // let week = env::args()
-    //     .collect::<Vec<_>>()
-    //     .get(1)
-    //     .expect("A week value to be supplied")
-    //     .parse()
-    //     .expect("To be a number");
     let client = EspnClient::build(SWID, ESPN_S2, LEAGUE_ID);
-    let teams = client.get_team_data(SEASON_ID).await;
+    let teams = client
+        .get_team_data(SEASON_ID)
+        .await;
 
     for week in 1..=5 {
-        let data = client.get_matchups_for_week(SEASON_ID, week, week).await;
+        let data = client
+            .get_matchups_for_week(SEASON_ID, week, week)
+            .await;
 
         println!("============== WEEK {week} ==================");
         for box_score in data {
@@ -46,22 +45,12 @@ fn get_unknowns(roster: Roster, team: TeamId, team_data: &TeamResponse) {
         .collect::<Vec<_>>();
     let team_detail = filt.get(0).unwrap();
     for player in roster.entries {
-        let stat = player
-            .player_pool_entry
-            .player
-            .stats
-            .expect("There are stats")
-            .get(0)
-            .expect("At least one")
-            .clone();
-
-        for val in stat.applied_stats {
-            if val.0.to_name() == "Unknown" && val.1 > 0.99 {
-                println!(
-                    "Team {:?} Player {} Stat Number {:?} Value {}",
-                    team_detail.name, player.player_pool_entry.player.full_name, val.0, val.1
-                )
-            }
+        let position = player.player_pool_entry.player.default_position_id;
+        if position.to_string() == "Unknown" {
+            println!(
+                "Team {:?} Player {} Position {:?}",
+                team_detail.name, player.player_pool_entry.player.full_name, position
+            )
         }
     }
 }
